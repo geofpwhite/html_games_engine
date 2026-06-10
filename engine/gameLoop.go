1fd70d822710ea2d177ff35072a6609222b66122
Geoffrey White <geoffpiercewhite@gmail.com>
2026-06-10 11:17:49 -0400

    make game loop synchronous, add mut for cleanup function, edit routes

--- engine/gameLoop.go
+++ engine/gameLoop.go
@@ -1,39 +1,76 @@
 package engine
 
 import (
+	"sync"
 	"time"
 
 	interfaces "github.com/geofpwhite/html_games_engine/interfaces"
 )
 
 func GameLoop(inputChannel <-chan interfaces.Input, outputChannel chan<- string, games map[string]interfaces.Game) {
-	var gameID string
-	var game interfaces.Game
 	lastModified := map[interfaces.Game]time.Time{}
+	var mu sync.Mutex
 	cleanupFunction := func() {
 		ticker := time.NewTicker(20 * time.Minute)
 		defer ticker.Stop()
-		var lastTick time.Time = time.Now()
+		lastTick := time.Now()
 		for interval := range ticker.C {
+			mu.Lock()
 			for id, game := range games {
 				if lastTick.Compare(lastModified[game]) > 0 {
-					//close the game
-					//hmm
+					// close the game
+					// hmm
 					delete(games, id)
-
 				}
 			}
+			mu.Unlock()
 			lastTick = interval
 		}
 	}
 	go cleanupFunction()
 	for userInput := range inputChannel {
-		gameID = userInput.GameID()
-		game = games[gameID]
-		go func(userInput interfaces.Input) {
-			userInput.ChangeState(game)
-			lastModified[game] = time.Now()
-			outputChannel <- gameID
-		}(userInput)
+		mu.Lock() // noop unless we are cleaning up
+		gameID := userInput.GameID()
+		game, ok := games[gameID]
+		if !ok {
+			mu.Unlock()
+			continue
+		}
+		userInput.ChangeState(game)
+		lastModified[game] = time.Now()
+		outputChannel <- gameID
+		mu.Unlock()
 	}
 }
+
+// func GameBus(inputChannel <-chan interfaces.Input, outputChannel chan<- string, games bus.EventBus[interfaces.Game]) {
+// 	var gameID string
+// 	var game interfaces.Game
+// 	lastModified := map[*bus.TopicBus[interfaces.Game]]time.Time{}
+// 	cleanupFunction := func() {
+// 		ticker := time.NewTicker(20 * time.Minute)
+// 		defer ticker.Stop()
+// 		var lastTick time.Time = time.Now()
+// 		for interval := range ticker.C {
+// 			for id, game := range games {
+// 				if lastTick.Compare(lastModified[game]) > 0 {
+// 					// close the game
+// 					// hmm
+// 					delete(games, id)
+// 				}
+// 			}
+// 			lastTick = interval
+// 		}
+// 	}
+// 	go cleanupFunction()
+// 	for userInput := range inputChannel {
+// 		gameID = userInput.GameID()
+// 		game = games[gameID]
+
+// 		go func(userInput interfaces.Input) {
+// 			userInput.ChangeState(game)
+// 			lastModified[game] = time.Now()
+// 			outputChannel <- gameID
+// 		}(userInput)
+// 	}
+// }

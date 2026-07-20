17feeeb4e12a5c08b50d7f0d85051dda48550464
Geoffrey White <geoffpiercewhite@gmail.com>
2026-07-20 18:18:52 -0400

    f

--- connect4/connect4Routes.go
+++ connect4/connect4Routes.go
@@ -2,6 +2,7 @@ package connect4
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"log/slog"
 	"net/http"
@@ -92,7 +93,7 @@ func Connect4Routes(r *http.ServeMux, tmpl *template.Template, upgrader *websock
 			return
 		}
 		game := (games[gameID]).(*connect4)
-		slog.Log(req.Context(), slog.LevelInfo, "%v", game)
+		slog.Log(req.Context(), slog.LevelInfo, "gameid: "+gameID, "value", fmt.Sprintf("%v", game))
 		rows := make([][]string, 8)
 		for i := range rows {
 			rows[i] = make([]string, 8)
